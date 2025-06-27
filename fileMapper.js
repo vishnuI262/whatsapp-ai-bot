fd445b07e54dd7dac71435f836d8580a8e078ba2
vishnuI262 <vishnusubramanyan2004@gmail.com>
2025-06-27 12:21:17 +0530

    Added Video

--- fileMapper.js
+++ fileMapper.js
@@ -3,8 +3,8 @@ module.exports = function fileMapper(query) {
     return {
       image: "https://whatsapp-ai-bot-yplb.onrender.com/assets/1680532048475.jpeg",
       pdf: "https://whatsapp-ai-bot-yplb.onrender.com/assets/CNN.pdf",
+      video: "https://whatsapp-ai-bot-yplb.onrender.com/assets/cnn.mp4",
     };
   }
-
   return {};
 };
